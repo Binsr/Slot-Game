eafb15a528651e9c12d53ee2ef77bbb0a68489c5
Binsr <urosjovanovic.sna@gmail.com>
2020-02-26 18:40:23 +0100

    Update

--- src/BetBtn.js
+++ src/BetBtn.js
@@ -3,7 +3,9 @@ export default class BetBtn{
         this.width= 100;
         this.height= 30;   
         this.color= "green";
-        this.bet= 10;
+        this.iterator= 0;
+        this.bets= [1,2,3,5,10,15,20];
+        this.bet= this.bets[this.iterator];
 
         this.position= {
             x: gameWidth - this.width,
@@ -15,12 +17,16 @@ export default class BetBtn{
         var d1= Math.sqrt(Math.pow(mouseX-70-this.position.x-this.width/3, 2) + Math.pow(mouseY-30-this.position.y, 2));//Udaljenost dve tacke
         var d2= Math.sqrt(Math.pow(mouseX+40-this.position.x-this.width/3, 2) + Math.pow(mouseY-30-this.position.y, 2));
         if(d1 < 30){
-            if(this.bet < 30)
-                this.bet+= 5;
+            if(this.iterator < this.bets.length-1){
+                this.iterator++;
+                this.bet= this.bets[this.iterator];
+            }
             return true;
         }else if(d2 < 20){
-            if(this.bet > 5)
-                this.bet-= 5;
+            if(this.iterator > 0){
+                this.iterator--;
+                this.bet= this.bets[this.iterator];
+            }
             return true;
         }
     }
