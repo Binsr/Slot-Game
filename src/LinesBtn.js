8b82bbf1c4a5fc286a0aed579e7b609ab8db64c2
Binsr <urosjovanovic.sna@gmail.com>
2020-03-02 03:57:43 +0100

    Update

--- src/LinesBtn.js
+++ src/LinesBtn.js
@@ -17,33 +17,7 @@ export default class LinesBtn{
             y: gameHeight - this.height,
         };
     }
-    draw(context){
-        context.moveTo(this.position.x-1, this.position.y+1);
-        context.lineTo(this.position.x+this.width, this.position.y+1);
-        context.stroke();
-        context.fillStyle= "black";
-        context.fillRect(this.position.x-this.width/3, this.position.y, this.width/3, this.height);
 
-        context.font="20pt bold default";
-        context.fillStyle = "yellow";
-        context.fillText("-",this.position.x-(this.width/3)/2,this.position.y+this.height/2);       
-        context.fillStyle= "black";
-        context.fillRect(this.position.x+this.width*2/3, this.position.y, this.width/3, this.height);
-        context.font="15pt bold default";
-        context.fillStyle = "yellow";
-        context.fillText("+",this.position.x+this.width-(this.width/6),this.position.y+this.height/2);
-        context.font="10pt arial";        
-        context.textAlign="center"; 
-        context.textBaseline = "middle";
-        context.fillStyle= "black";
-        context.fillText(this.linesActive.toString(), this.position.x+this.width/3, this.position.y+this.height/2);
-        context.fillText("Lines",this.position.x+this.width/3,this.position.y-6);
-        context.moveTo(this.position.x-this.width/3+1, this.position.y);
-        context.lineTo(this.position.x-this.width/3+1, this.position.y-15);
-        context.lineTo(this.position.x+this.width-1, this.position.y-15);
-        context.lineTo(this.position.x+this.width-1, this.position.y);
-        context.stroke();
-    }
     clicked(mouseX,mouseY){
         var d1= Math.sqrt(Math.pow(mouseX-70-this.position.x-this.width/3, 2) + Math.pow(mouseY-30-this.position.y, 2));//Udaljenost dve tacke
         var d2= Math.sqrt(Math.pow(mouseX+40-this.position.x-this.width/3, 2) + Math.pow(mouseY-30-this.position.y, 2));
@@ -81,5 +55,32 @@ export default class LinesBtn{
         return this.linesActive;
     }
 
+    draw(context){
+        context.moveTo(this.position.x-1, this.position.y+1);
+        context.lineTo(this.position.x+this.width, this.position.y+1);
+        context.stroke();
+        context.fillStyle= "black";
+        context.fillRect(this.position.x-this.width/3, this.position.y, this.width/3, this.height);
+
+        context.font="20pt bold default";
+        context.fillStyle = "yellow";
+        context.fillText("-",this.position.x-(this.width/3)/2,this.position.y+this.height/2);       
+        context.fillStyle= "black";
+        context.fillRect(this.position.x+this.width*2/3, this.position.y, this.width/3, this.height);
+        context.font="15pt bold default";
+        context.fillStyle = "yellow";
+        context.fillText("+",this.position.x+this.width-(this.width/6),this.position.y+this.height/2);
+        context.font="10pt arial";        
+        context.textAlign="center"; 
+        context.textBaseline = "middle";
+        context.fillStyle= "black";
+        context.fillText(this.linesActive.toString(), this.position.x+this.width/3, this.position.y+this.height/2);
+        context.fillText("Lines",this.position.x+this.width/3,this.position.y-6);
+        context.moveTo(this.position.x-this.width/3+1, this.position.y);
+        context.lineTo(this.position.x-this.width/3+1, this.position.y-15);
+        context.lineTo(this.position.x+this.width-1, this.position.y-15);
+        context.lineTo(this.position.x+this.width-1, this.position.y);
+        context.stroke();
+    }
 
 }
