a7ece80499361619bc51eceac4af47060a0e0915
Binsr <urosjovanovic.sna@gmail.com>
2020-03-02 03:45:26 +0100

    Update

--- src/Calculations.js
+++ src/Calculations.js
@@ -40,7 +40,6 @@ export default class Calculations{
 
                     }
                 }
-                console.log(oneLineWin);
                 win+= oneLineWin;
             }
         }
